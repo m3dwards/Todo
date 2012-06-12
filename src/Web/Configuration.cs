aee6263f713a0b35f3c9fd1bab94239bc740a621
Max Edwards <youwontforgetthis@gmail.com>
2012-06-12 22:10:05 +0100

    First Commit

---
+++ src/Web/Configuration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using OpenRasta.Configuration;
+using Web.Resources;
+using Web.Handlers;
+using OpenRasta.Codecs.Razor;
+//using OpenRasta.Hosting.AspNet;
+
+namespace OpenRastaSample
+{
+    public class Configuration : IConfigurationSource
+    {
+        public void Configure()
+        {
+            using (OpenRastaConfiguration.Manual)
+            {
+                ResourceSpace.Has.ResourcesOfType<Home>()
+                .AtUri("/home")
+                .HandledBy<HomeHandler>()
+                .RenderedByRazor("/views/Home.cshtml");
+            }
+        }
+    }
+}
---
+++ src/Web/Handlers/HomeHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Web.Resources;
+
+namespace Web.Handlers
+{
+    public class HomeHandler
+    {
+
+        public Home Get()
+        {
+            return new Home
+            {
+                Title = "Hello, OpenRasta! Time to rock!"
+            };
+        }
+
+        public Home Post(Home model)
+        {
+            return Get();
+        }
+
+    }
+}
